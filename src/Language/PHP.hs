2ee88737ef9c7a228369ec26f706b839eb069b5e
Daan Rijks <daanrijks@gmail.com>
2017-12-10 00:01:21 +0100

    Define operators
    Because why the fuck not.

--- src/Language/PHP.hs
+++ src/Language/PHP.hs
@@ -1,3 +1,84 @@
 module Language.PHP where
 
 hello = "<?php echo \"hello, world!\""
+
+data BinOp
+    -- Arithmetic
+    = Add -- +
+    | Subtract -- -
+    | Multiply -- *
+    | Divide -- /
+    | Modulo -- %
+    | Exponentiate -- **
+
+    -- Logical
+    | And -- &&
+    | Or -- ||
+    | Xor -- xor
+    | AndAlt -- and
+    | OrAlt -- or
+
+    -- Comparison
+    | Equal -- ==
+    | Identical -- ===
+    | NotEqual -- !=
+    | NotEqualAlt -- <> -- Because SQL or something.
+    | NotIdentical -- !==
+    | LessThan -- <
+    | GreaterThan -- >
+    | LessThanEqual -- <=
+    | GreaterThanEqual -- >=
+    -- | Spaceship -- <=> apparently, and only in PHP 7.
+    -- These people were smoking some good stuff when they came up with this shit.
+
+    -- Bitwise
+    | BitwiseAnd -- &
+    | BitwiseOr -- |
+    | BitwiseXor -- ^
+    | ShiftLeft -- <<
+    | ShiftRight -- >>
+
+    -- String Manipulation
+    | Concat -- .
+
+    -- Assignment
+    | Assign -- =
+    | AddAssign -- +=
+    | SubtractAssign -- -=
+    | MultiplyAssign -- *=
+    | DivideAssign -- /=
+    | ModuloAssign -- %=
+    | ConcatAssign -- .=
+    | BWAndAssign -- &=
+    | BWOrAssign -- |=
+    | BWXorAssign -- ^=
+    | LeftShiftAssign -- <<=
+    | RightShiftAssign -- >>=
+
+    -- Types
+    | InstanceOf -- instanceof
+
+data UnOp
+    -- Arithmetic
+    = Identity -- +
+    | Negate -- -
+
+    -- Impure Arithmetic
+    | PreIncrement -- ++
+    | PostIncrement
+    | PreDecrement -- --
+    | PostDecrement
+
+    -- Logical
+    | Not -- !
+
+    -- Bitwise
+    | BitwiseNot -- ~
+
+    -- Error Control
+    | Ignore -- @
+    -- Because why not make `try { foo(); } catch () {}` an operator...
+
+data Expr
+    = BinOp BinOp Expr Expr
+    | UnOp UnOp Expr Expr
