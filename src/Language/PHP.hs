f44f2b012f0a4321fd67c0d8e59ed2731c0f8708
Daan Rijks <daanrijks@gmail.com>
2017-12-10 21:41:01 +0100

    Add basic pretty printing

--- src/Language/PHP.hs
+++ src/Language/PHP.hs
@@ -1,4 +1,9 @@
-module Language.PHP where
+module Language.PHP
+    ( module Language.PHP.AST
+    , module Language.PHP.Pretty
+    ) where
+
+import Language.PHP.AST
+import Language.PHP.Pretty
 
 hello = "<?php echo \"hello, world!\""
-
---
+++ src/Language/PHP/Pretty.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.PHP.Pretty where
+
+import Data.Char (toLower)
+import Data.Semigroup
+
+import Text.PrettyPrint (Doc, (<+>))
+import qualified Text.PrettyPrint as PP
+
+import Language.PHP.AST
+
+class Pretty p where
+    pretty :: p -> PP.Doc
+
+instance Pretty UnOp where
+    pretty op =
+        case op of
+            Identity -> "+"
+            Negate -> "-"
+            Not -> "!"
+
+instance Pretty BinOp where
+    -- TODO: just use a lookup table for this so that parsing is easier too
+    pretty op =
+        case op of
+            Add -> "+"
+            Subtract -> "-"
+            Multiply -> "*"
+            Divide -> "/"
+            Modulo -> "%"
+            Exponentiate -> "**"
+            _ -> error "unimplemented"
+
+instance Pretty Literal where
+    pretty (Int i) = PP.int i
+    pretty (Float f) = PP.float f
+    pretty (Bool b) = PP.text $ map toLower $ show b
+    pretty (SingleQuotes s) = PP.quotes $ PP.text s
+    pretty (DoubleQuotes s) = PP.doubleQuotes $ PP.text s
+    pretty (Backticks s) = "`" <> PP.text s <> "`"
+    pretty (Null) = "NULL"
+
+instance Pretty Expr where
+    pretty (BinOp op lhs rhs) = PP.parens (pretty lhs <+> pretty op <+> pretty rhs)
+    pretty (UnOp op e) = pretty op <> PP.parens (pretty e)
+    pretty (Literal l) = pretty l
+    pretty _ = error "unimplemented"
